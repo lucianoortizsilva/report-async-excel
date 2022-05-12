5cb93da3a184f1b1a7537d66affe99e4c99d59b2
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-05-12 19:43:29 -0300

    Atualizando configuracoes

--- report-api/entrypoint.sh
+++ report-api/entrypoint.sh
@@ -4,7 +4,7 @@ while ! nc -z container-rabbitmq 5672 ; do
     echo "###############################################"
     echo "######## Aguardando container-rabbitmq ########"
     echo "###############################################"
-    sleep 30
+    sleep 20
 done
 
 /report-api
