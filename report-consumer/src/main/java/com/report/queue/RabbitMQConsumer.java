ade210ed37cbced4a6ef410739bff5188b9900d3
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-05-11 22:31:00 -0300

    Alterando report-consumer para converter JSON to Object

--- report-consumer/src/main/java/com/report/queue/RabbitMQConsumer.java
+++ report-consumer/src/main/java/com/report/queue/RabbitMQConsumer.java
@@ -2,6 +2,7 @@ package com.report.queue;
 
 import java.io.File;
 
+import org.json.JSONObject;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,15 +23,19 @@ public class RabbitMQConsumer {
 	private EmailService emailService;
 
 	@RabbitListener(queues = "QUEUE-REPORT")
-	public void consumerQueue(final Report report) {
+	public void consumerQueue(final String message) {
 		log.info("> Mensagem recebida da fila: QUEUE-REPORT");
 		try {
+			final JSONObject json = new JSONObject(message);
+			Report report = new Report();
+			report.setEmailTo(json.getString("emailTo"));
+			report.setReleaseYear(json.getInt("releaseYear"));
 			long inicio = System.currentTimeMillis();
-			log.info("> Gerando relatório");
+			log.info("> Gerando relatorio");
 			final File file = this.service.generateFile(report.getReleaseYear());
 			long tempo = System.currentTimeMillis() - inicio;
 			log.info("> Tempo gasto em milissegundos: {}", tempo);
-			log.info("> Relatório gerado: {}", file.getName());
+			log.info("> Relatorio gerado: {}", file.getName());
 			log.info("> Path: {}", file.getAbsoluteFile());
 			emailService.sendMailWithReport(report, file);
 		} catch (final Exception e) {
