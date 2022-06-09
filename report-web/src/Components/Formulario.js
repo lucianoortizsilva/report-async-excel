5db51ebb67a67ab130b8bdac1993002aac8bdabf
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-08 21:18:51 -0300

    Add botao com spinner

--- report-web/src/Components/Formulario.js
+++ report-web/src/Components/Formulario.js
@@ -1,73 +1,76 @@
-import React, {Component} from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import React, {useState} from 'react';
+import { Button, Container, Form, Spinner } from "react-bootstrap";
 
-class Formulario extends Component{
+function Formulario() {
 
-    constructor(props){
-        super(props)
-        this.state = {
-            formulario: {
-                email: '',
-                ano: ''
-            },
-            anos: ['2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015']
-        }
-        this.enviar = this.enviar.bind(this)
-    }
+    const anos = ['2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015'];
+    const [isLoading, setLoading] = useState(false);
+    const [ano, setAno] = useState('');
+    const [email, setEmail] = useState('');    
+    const [labelBotao, setLabelBotao] = useState('Enviar');
+    const [spinnerBotao, setSpinnerBotao] = useState('visually-hidden');
 
-    render(){
-        return(
-            <div className="mt-5">
-                <Container>
-                    <Form className="mb-8" onSubmit={this.enviar}>
-                        
-                        <Form.Group className="mb-3" controlId="email">
-                            <Form.Label className="fw-bold">E-mail</Form.Label>
-                            <Form.Control name="email" 
-                                          type="email" 
-                                          value={this.state.formulario.email}
-                                          onChange={this.enviar} 
-                                          placeholder="Informe o e-mail para enviar o relatório" 
-                                          required/>
-                        </Form.Group>
-                        
-                        <Form.Group className="mb-3">
-                            <Form.Label className="fw-bold" htmlFor="ano">Ano de Lançamento</Form.Label>
-                            <Form.Select name="ano" 
-                                         value={this.state.formulario.ano}
-                                         onChange={this.enviar}                                          
-                                         placeholder="Selecione ..."
-                                         required>
 
-                                <option value="" disabled>Selecione ...</option>
-                                {
-                                    this.state.anos.map((obj) => {
-                                        return(
-                                            <option key={obj}>
-                                                {obj}
-                                            </option>
-                                        )
-                                    })
-                                }                               
-                            </Form.Select>
-                        </Form.Group>
 
-                        <Button type="submit">Enviar</Button>
-                    </Form>
-                </Container>
-            </div>
-        )
-    }
 
-    enviar(event){
-        let form = this.state.formulario;                
-        form[event.target.name] = event.target.value;        
-        this.setState({formulario:form})
-        if(event.type === 'submit'){
-            console.log('Email...: ' + form.email);        
-            console.log('Ano.....: ' + form.ano);
-            alert('Dados enviados com sucesso!');
-        }
+
+    return(
+        <div className="mt-5">
+            <Container>
+                <Form className="mb-8" onSubmit={enviar}>
+                    
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label className="fw-bold">E-mail</Form.Label>
+                        <Form.Control name="email" 
+                                        type="email" 
+                                        value={email}
+                                        onChange={(e)=> setEmail(e.target.value)}
+                                        placeholder="Informe o e-mail para enviar o relatório" 
+                                        required/>
+                    </Form.Group>
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label className="fw-bold" htmlFor="ano">Ano de Lançamento</Form.Label>
+                        <Form.Select name="ano" 
+                                        value={ano}
+                                        onChange={(e)=> setAno(e.target.value)}
+                                        placeholder="Selecione ..."
+                                        required>
+
+                            <option value="" disabled>Selecione ...</option>
+                            {
+                                anos.map((obj) => {
+                                    return(
+                                        <option key={obj}>
+                                            {obj}
+                                        </option>
+                                    )
+                                })
+                            }                               
+                        </Form.Select>
+                    </Form.Group>
+                    <Button variant="primary" type='submit' disabled={isLoading}>
+                        <Spinner as="span" 
+                                 variant='danger' 
+                                 animation="grow" 
+                                 size="sm" 
+                                 role="status"
+                                 aria-hidden="false"
+                                 className={spinnerBotao}
+                                 disabled={isLoading}/> {labelBotao}
+                    </Button>
+                </Form>
+            </Container>
+        </div>
+    )
+
+    function enviar(){
+        setLoading(true);
+        setSpinnerBotao('');
+        setLabelBotao('Enviando');
+        console.log('Email...: ' + email);
+        console.log('Ano.....: ' + ano);
+        console.log('Dados enviados com sucesso!');
     }
 
 }
