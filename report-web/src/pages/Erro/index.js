b5a0f795b074a8df44ca1043bde989c349bb5341
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-06-10 14:22:09 -0300

    Add melhorias na tela de erro

--- report-web/src/pages/Erro/index.js
+++ report-web/src/pages/Erro/index.js
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
+import erro from '../../assets/erro.png'
+import { Link, useNavigate  } from 'react-router-dom';
+import '../Erro/style.css'
+
+
 function Erro() {
+
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+      setTimeout(()=> {
+        navigate("/", { replace: true });
+      }, 5000)
+    });
+
     return (
-      <div>
-        <h3>Erro inesperado!!!</h3>
+      <div className='pnl-erro'>
+        <img src={erro} className="d-inline-block align-top"/>
+        <h2>Pagina não encontrada!</h2>
+        <Link to="/">HOME</Link>
       </div>
     );
   }
   
-  export default Erro;
+export default Erro;
